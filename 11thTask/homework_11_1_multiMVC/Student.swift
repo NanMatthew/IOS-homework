d4940b8794ed76193ef2d1614d5b8686c1c3fa16
NanMatthew <42998308+NanMatthew@users.noreply.github.com>
2018-12-18 14:05:23 +0800

    第十一次实验

---
+++ 11thTask/homework_11_1_multiMVC/Student.swift
@@ -0,0 +1,13 @@
+//
+//  Student.swift
+//  homework_11_1_multiMVC
+//
+//  Created by student on 2018/12/15.
+//  Copyright © 2018年 2016070609. All rights reserved.
+//
+
+import Foundation
+class Student {
+    var no = ""
+    var name = ""
+}
